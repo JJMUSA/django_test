438f6d3565985a676204bd8931a070375d50d796
JJMUSA <thejabirmusa@gmail.com>
2022-06-23 12:58:02 +0100

    update previous reservation querying and querying for rental_name

--- rental_reservation/views.py
+++ rental_reservation/views.py
@@ -2,6 +2,7 @@
 from .models import Rental, Reservation
 from django.shortcuts import render
 from django.db.models import Subquery, OuterRef, F
+from django.db.models.functions import Coalesce
 # Create your views here.
 
 
@@ -10,7 +11,7 @@ def get_reservations(request):
                                                             rental_id=OuterRef('rental_id')).order_by('checkin')
 
     all_reservation = Reservation.objects.annotate(rental_name=F('rental_id__name'),
-                                                   previous_rev=Subquery(previous_reservation_query.values('id')[:1]))
+                                                   previous_rev=Subquery(previous_reservation_query.values('id').reverse()[:1]))
 
     table = [{'Rental_name': rev.rental_name, 'ID': rev.id,
               'Checkin': rev.checkin.strftime('%Y-%m-%d'), 'Checkout': rev.checkout.strftime('%Y-%m-%d'),
