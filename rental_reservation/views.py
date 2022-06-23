c87bad600cfce55e20f345052bd97c505fd78499
JJMUSA <thejabirmusa@gmail.com>
2022-06-23 12:41:05 +0100

    update previous reservation querying and querying for rental_name

--- rental_reservation/views.py
+++ rental_reservation/views.py
@@ -1,13 +1,19 @@
 
 from .models import Rental, Reservation
 from django.shortcuts import render
+from django.db.models import Subquery, OuterRef, F
 # Create your views here.
 
 
 def get_reservations(request):
-    all_reservation = Reservation.objects.all()
-    table = [{'Rental_name': rev.rental_id.name, 'ID': rev.id,
+    previous_reservation_query = Reservation.objects.filter(checkin__lt=OuterRef('checkin'),
+                                                            rental_id=OuterRef('rental_id')).order_by('checkin')
+
+    all_reservation = Reservation.objects.annotate(rental_name=F('rental_id__name'),
+                                                   previous_rev=Subquery(previous_reservation_query.values('id')[:1]))
+
+    table = [{'Rental_name': rev.rental_name, 'ID': rev.id,
               'Checkin': rev.checkin.strftime('%Y-%m-%d'), 'Checkout': rev.checkout.strftime('%Y-%m-%d'),
-              "Last_reservation": rev.previous_reservation} for rev in all_reservation]
+              "Last_reservation": rev.previous_rev} for rev in all_reservation]
     context = {'reservation_table': table}
     return render(request, 'reservation_list.html', context)
